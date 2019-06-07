02c0bc0c276497be5989f36b284c87ecda9a1703
lazerlasse <31047935+lazerlasse@users.noreply.github.com>
2019-06-07 09:36:15 +0200

    Initial create...

---
+++ KaffeMaskine/MainParts/CoffeeMaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaffeMaskine
+{
+	class CoffeeMaker
+	{
+		public bool IsOn { get; set; }
+		public int WaterContainer { get; set; }
+	}
+}
